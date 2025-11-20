43e928303b127a69066fda2a4ea5f6d2b02c462d
neeesa05-create <neeesa05@gmail.com>
2025-11-20 17:49:18 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,19 +1,6 @@
-export const config = {
-  runtime: 'edge',
-};
-
-export default function handler(req) {
-  return new Response(
-    JSON.stringify({
-      supabaseUrl: process.env.SUPABASE_URL,
-      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
-      googleClientId: process.env.GOOGLE_CLIENT_ID,
-    }),
-    {
-      status: 200,
-      headers: {
-        'content-type': 'application/json',
-      },
-    }
-  );
+export default function handler(request, response) {
+    response.status(200).json({
+        supabaseUrl: process.env.SUPABASE_URL,
+        supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    });
 }
